3c8bbc91ac104d0517374d813511169ccdc2674b
buckle2000 <buckle2000@zoho.com>
2016-12-23 14:51:18 +0800

    Vue.js version

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#app',
+//data: {
+//  prize: '瞎鸡巴乱跳奖',
+//  winner: '安伟力',
+//  action: '卓越的跳跃能力',
+//  message: '无',
+//  image: 'https://www.collegeboard.org/sites/default/files/Percent_156.png', 
+//}
+	data: {
+		action:  '标化考试成绩的趋势',
+		winner:  '*神',
+		prize:   '线性英语',
+		message: '“没有110啊”',
+	}
+})
+
